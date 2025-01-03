4206cbc1518d03d0807c40b8d2f9ef7bf351a52c
Xaruh007 <147065692+Xaruh007@users.noreply.github.com>
2025-01-03 20:03:38 +0100

    Update DatabaseConnection.java

--- CODE PROJET JAVA/util/DatabaseConnection.java
+++ CODE PROJET JAVA/util/DatabaseConnection.java
@@ -1,13 +1,12 @@
-package util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
     public static Connection getConnection() throws SQLException {
-        String url = "jdbc:mysql://localhost:3306/camp_activites";
-        String user = "root";
-        String password = "z22ckgw0";
+        String url = "jdbc:mysql://localhost:3306/camp_activites"; // Port 3306 par défaut pour Laragon
+        String user = "root"; // Par défaut dans Laragon
+        String password = "123"; // Mot de passe vide par défaut dans Laragon
         return DriverManager.getConnection(url, user, password);
     }
 }
